9562860d6d5f98c60ac9beb42146328531e13965
EnyewAnberbir <119450772+EnyewAnberbir@users.noreply.github.com>
2023-12-13 17:19:17 +0300

    Added solution for Reverse Words in a String

---
+++ leet-code-solutions/reverse-words-in-a-string.py
@@ -0,0 +1,23 @@
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        reversed_string = reversed(s.split())
+        
+        
+        return " ".join(reversed_string)
+
+        
+        
+
+
+
+
+
+            
+
+       
+
+
+
+
+
+        
